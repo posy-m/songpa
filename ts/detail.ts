ca09fa89416c86537aa95c112650d35ef7bc7b0e
pvekxx <https://github.com/pvekxx>
2024-05-17 21:45:59 +0900

    댓글 구현

--- ts/detail.ts
+++ ts/detail.ts
@@ -22,12 +22,28 @@ class DetailRenderManager {
         }
     }
 
+    // 댓글 배열 푸시후 댓글배열 로컬에 생성 
+    setLocalStorage(a) {
+        this.boardDataList.push(a);
+        localStorage.setItem("reply_data", JSON.stringify(this.boardDataList))
+    }
+
+    getLocalStorage(a) {
+        if (a === null) {
+            localStorage.setItem("reply_data", JSON.stringify(this.boardDataList));
+        } else {
+            this.boardDataList = JSON.parse(a);
+        }
+    }
+
     render() {
         // this.getLocalStorage(localStorage.getItem("board_data"))
+        this.getLocalStorage(localStorage.getItem("reply_data"));
         this.getsessionStorage(sessionStorage.getItem("login_status"));
 
         const param = new URLSearchParams(location.search).get("index");
 
+        // 보드 데이터
         const localdata = JSON.parse(localStorage.getItem("board_data"))[param];
         const detail_title = localdata.title;
         const detail_userName = localdata.userName;
@@ -39,14 +55,43 @@ class DetailRenderManager {
         const date = <HTMLElement>document.querySelector(".date");
         const content = <HTMLElement>document.querySelector("#content");
 
+        const reply = <HTMLInputElement>document.querySelector("#reply");
+
         const btnDelete = <HTMLElement>document.querySelector("#btn_delete");
         const btnBoard = <HTMLElement>document.querySelector("#btn_board");
         const btnConfirm = <HTMLElement>document.querySelector("#btn_confirm");
+        const btnReply = <HTMLElement>document.querySelector("#btn_reply");
 
         title.innerHTML = detail_title;
         userName.innerHTML = detail_userName;
         date.innerHTML = detail_date;
         content.innerHTML = detail_content;
+
+
+        // 댓글 데이터
+        for (let i = 0; i < (JSON.parse(localStorage.getItem("reply_data"))).length; i++) {
+            if ((JSON.parse(localStorage.getItem("reply_data"))[i].replyindex) == param) {
+                const localReplyData = JSON.parse(localStorage.getItem("reply_data"))[i];
+
+                const detail_replyUserName = localReplyData.replyUserName;
+                const detail_reply = localReplyData.reply;
+                const detail_replydate = localReplyData.replydate;
+
+                const replyList = <HTMLElement>document.querySelector("#replylist");
+
+                const replyWriter = <HTMLElement>document.createElement("span");
+                const replyDetail = <HTMLElement>document.createElement("span");
+                const replyDate = <HTMLElement>document.createElement("span");
+                const replyContent = <HTMLElement>document.createElement("div");
+
+                replyWriter.innerHTML = detail_replyUserName;
+                replyDetail.innerHTML = detail_reply;
+                replyDate.innerHTML = detail_replydate;
+                replyContent.append(replyWriter, replyDetail, replyDate);
+                replyList.append(replyContent);
+            }
+        }
+
 
         // 목록으로 버튼
         btnBoard.onclick = () => {
@@ -67,6 +112,29 @@ class DetailRenderManager {
                 location.href = "./board.html?index=0";
             } else {
                 return;
+            }
+        }
+
+        // 댓글 달기 버튼 회원이 아닐경우 댓 버튼 클릭시 얼러트
+        btnReply.onclick = () => {
+            if (JSON.stringify(sessionStorage.getItem("login_status")) == `"{}"`) {
+                alert("로그인을 해주세요!");
+            } else if (reply.value === "") {
+                alert("댓글을 입력해주세요!")
+            } else {
+                const date = new Date();
+                const year = date.getFullYear();
+                const month = date.getMonth() + 1;
+                const day = date.getDate();
+                const replyData = {
+                    replyUserName: (JSON.parse(sessionStorage.getItem("login_status"))).userName,
+                    reply: reply.value,
+                    replydate: `${year}-${month}-${day}`,
+                    replyindex: param
+                }
+                // 로컬스토리지로 저장
+                this.setLocalStorage(replyData);
+                location.href = "./detail.html?index=" + param;
             }
         }
 
